2e014741fe5c430dc133f8717bb11b0167952aac
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-12 19:08:07 -0700

    openssh: don't spam warnings as errors
    Change-Id: I88dbf17aa4401f2babc20806f1ac8a9f8ff1b842

--- Android.mk
+++ Android.mk
@@ -136,6 +136,8 @@ ifneq (,$(SSHDIR))
 LOCAL_CFLAGS += -DSSHDIR=\"$(SSHDIR)\"
 endif
 
+LOCAL_CFLAGS += -Wno-sign-compare -Wno-error
+
 include $(BUILD_SHARED_LIBRARY)
 
 ###################### ssh ######################
@@ -156,7 +158,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MODULE := ssh
 
-LOCAL_CFLAGS += -Wno-unused-parameter -Wno-macro-redefined
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-macro-redefined -Wno-error
 
 LOCAL_C_INCLUDES := \
     external/zlib \
@@ -263,7 +265,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MODULE := sshd
 
-LOCAL_CFLAGS += -Wno-unused-parameter -Wno-macro-redefined
+LOCAL_CFLAGS += -Wno-error -Wno-unused-parameter -Wno-macro-redefined
 
 LOCAL_C_INCLUDES := \
     external/zlib \
