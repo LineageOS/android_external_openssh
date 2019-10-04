08bccbef39dd763b2b00a2c674850b360c815ade
Michael Bestas <mkbestas@lineageos.org>
2019-10-04 19:01:42 +0300

    Revert "openssh: Silence build warnings"
    * Replaced by upstream commit
    
    This reverts commit 729a90637113546fcbb529b815f704bc9f67b8f7.
    
    Change-Id: Ia6a5cc79ddc9ff7a693cba23dbb28b4c652f7e04

--- Android.mk
+++ Android.mk
@@ -125,12 +125,7 @@ LOCAL_SHARED_LIBRARIES += libssl libcrypto libdl libz
 
 LOCAL_MODULE := libssh
 
-LOCAL_CFLAGS += \
-    -O3 \
-    -Wno-macro-redefined \
-    -Wno-pointer-sign \
-    -Wno-sign-compare \
-    -Wno-unused-parameter
+LOCAL_CFLAGS += -O3 -Wno-unused-parameter -Wno-macro-redefined
 
 LOCAL_CFLAGS += -DGCE_PLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
 ifneq ($(filter gce_x86 calypso, $(TARGET_DEVICE)),)
@@ -140,6 +135,8 @@ endif
 ifneq (,$(SSHDIR))
 LOCAL_CFLAGS += -DSSHDIR=\"$(SSHDIR)\"
 endif
+
+LOCAL_CFLAGS += -Wno-sign-compare -Wno-error
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -161,11 +158,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MODULE := ssh
 
-LOCAL_CFLAGS += \
-    -Wno-macro-redefined \
-    -Wno-pointer-sign \
-    -Wno-sign-compare \
-    -Wno-unused-parameter
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-macro-redefined -Wno-error
 
 LOCAL_C_INCLUDES := \
     external/zlib \
@@ -269,12 +262,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MODULE := sshd
 
-LOCAL_CFLAGS += \
-    -Wno-implicit-function-declaration \
-    -Wno-macro-redefined \
-    -Wno-pointer-sign \
-    -Wno-unused-parameter \
-    -Wno-unused-variable
+LOCAL_CFLAGS += -Wno-error -Wno-unused-parameter -Wno-macro-redefined
 
 LOCAL_C_INCLUDES := \
     external/zlib \
