8850011eb7cebf34263df29b78ab0287d03aff18
Luke Dashjr <luke-jr+git+lineageos@utopios.org>
2022-04-17 03:11:36 +0000

    Bugfix: pwcopy: Tolerate NULL pw_{gecos,class,dir,shell} fields
    Change-Id: I520aa646d69cbce398bfd0a82c0df9f510186268

--- misc.c
+++ misc.c
@@ -462,7 +462,7 @@ pwcopy(struct passwd *pw)
 	copy->pw_name = xstrdup(pw->pw_name);
 	copy->pw_passwd = xstrdup(pw->pw_passwd == NULL ? "*" : pw->pw_passwd);
 #ifdef HAVE_STRUCT_PASSWD_PW_GECOS
-	copy->pw_gecos = xstrdup(pw->pw_gecos);
+	if (pw->pw_gecos) copy->pw_gecos = xstrdup(pw->pw_gecos);
 #endif
 	copy->pw_uid = pw->pw_uid;
 	copy->pw_gid = pw->pw_gid;
@@ -473,10 +473,10 @@ pwcopy(struct passwd *pw)
 	copy->pw_change = pw->pw_change;
 #endif
 #ifdef HAVE_STRUCT_PASSWD_PW_CLASS
-	copy->pw_class = xstrdup(pw->pw_class);
+	if (pw->pw_class) copy->pw_class = xstrdup(pw->pw_class);
 #endif
-	copy->pw_dir = xstrdup(pw->pw_dir);
-	copy->pw_shell = xstrdup(pw->pw_shell);
+	if (pw->pw_dir) copy->pw_dir = xstrdup(pw->pw_dir);
+	if (pw->pw_shell) copy->pw_shell = xstrdup(pw->pw_shell);
 	return copy;
 }
 
